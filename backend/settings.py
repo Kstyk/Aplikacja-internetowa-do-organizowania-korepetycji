4f086e36aab392b07cfbb6a7100e80f76748affe
Seweryn Drag <dragseweryn@zohomail.eu>
2023-07-24 16:11:26 +0200

    Zainstalowanie paczki django cleanup pomocnej do czyszczenia zawartosci storage wraz z usuwaniem linku w bazie do danego pliku

--- backend/settings.py
+++ backend/settings.py
@@ -45,8 +45,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'django_cleanup.apps.CleanupConfig',
     'rooms',
-    'classes'
+    'classes',
 ]
 
 REST_FRAMEWORK = {
@@ -57,7 +58,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=150),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
