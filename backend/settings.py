062068b74ccda4a38d18b8a3e691922a4a0b007d
Seweryn Drag <dragseweryn@zohomail.eu>
2023-08-02 11:58:53 +0200

    Konfiguracja cities-light - miasta powyzej 500 mieszkańców w Polsce

--- backend/settings.py
+++ backend/settings.py
@@ -215,3 +215,5 @@ CITIES_LIGHT_TRANSLATION_LANGUAGES = ['pl']
 CITIES_LIGHT_INCLUDE_COUNTRIES = ['PL']
 CITIES_LIGHT_INCLUDE_CITY_TYPES = ['PPL', 'PPLA', 'PPLA2', 'PPLA3',
                                    'PPLA4', 'PPLC', 'PPLF', 'PPLG', 'PPLL', 'PPLR', 'PPLS', 'STLMT']
+CITIES_LIGHT_CITY_SOURCES = [
+    "http://download.geonames.org/export/dump/cities500.zip"]
