e93b094c769120a130235e7baa13fcb464afabb3
Seweryn Drag <dragseweryn@zohomail.eu>
2023-08-04 20:59:34 +0200

    Dodanie walidatora roli studenta

--- classes/validators.py
+++ classes/validators.py
@@ -23,6 +23,26 @@ def validate_teacher_role(value):
                 "Tylko rola 'nauczyciel' może być przypisana do zajęć.")
 
 
+def validate_student_role(value):
+    User = get_user_model()
+
+    try:
+        if type(value) is int:
+            user = User.objects.get(pk=value)
+        else:
+            user = User.objects.get(pk=value.id)
+    except User.DoesNotExist:
+        raise ValidationError("Użytkownik o podanym ID nie istnieje.")
+
+    if user.role is None:
+        raise ValidationError(
+            "Użytkownik nie posiada roli 'student'.")
+    else:
+        if user.role.name != "Student":
+            raise ValidationError(
+                "Użytkownik nie posiada roli 'student'.")
+
+
 def validate_future_date(value):
     if value <= date.today():
         raise ValidationError(
