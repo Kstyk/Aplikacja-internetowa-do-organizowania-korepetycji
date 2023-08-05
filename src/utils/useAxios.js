88729fb7a62cd116fd933062c1f832b66888611e
Seweryn Drag <dragseweryn@zohomail.eu>
2023-08-05 19:09:51 +0200

    Poprawa hooka useAxios dla niezalogowanych userow

--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -9,30 +9,37 @@ const baseURL = "http://127.0.0.1:8000";
 const useAxios = () => {
   const { authTokens, setUser, setAuthTokens } = useContext(AuthContext);
 
-  const axiosInstance = axios.create({
-    baseURL,
-    headers: {
-      Authorization: `Bearer ${authTokens?.access}`,
-    },
-  });
+  const axiosInstance =
+    authTokens != null
+      ? axios.create({
+          baseURL,
+          headers: {
+            Authorization: `Bearer ${authTokens?.access}`,
+          },
+        })
+      : axios.create({
+          baseURL,
+        });
 
   axiosInstance.interceptors.request.use(async (req) => {
-    const user = jwtDecode(authTokens.access);
-    const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+    if (authTokens != null) {
+      const user = jwtDecode(authTokens?.access);
+      const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
 
-    if (!isExpired) return req;
+      if (!isExpired) return req;
 
-    const response = await axios.post(`${baseURL}/api/auth/token/refresh/`, {
-      refresh: authTokens.refresh,
-    });
+      const response = await axios.post(`${baseURL}/api/auth/token/refresh/`, {
+        refresh: authTokens?.refresh,
+      });
 
-    localStorage.setItem("authTokens", JSON.stringify(response.data));
+      localStorage.setItem("authTokens", JSON.stringify(response.data));
 
-    setAuthTokens(response.data);
-    setUser(jwtDecode(response.data.access));
+      setAuthTokens(response.data);
+      setUser(jwtDecode(response.data.access));
 
-    req.headers.Authorization = `Bearer ${response.data.access}`;
-    return req;
+      req.headers.Authorization = `Bearer ${response.data.access}`;
+      return req;
+    } else return req;
   });
 
   return axiosInstance;
