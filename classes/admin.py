03f46f43b0d1f7b0f45d3fc3bb34bb3d886c5f04
Seweryn Drag <dragseweryn@zohomail.eu>
2023-08-07 19:08:56 +0200

    Dodanie modeli TimeSlot i PurchaseHistory do panelu admina

--- classes/admin.py
+++ classes/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Class, Language, TypeOfClasses, Schedule
+from .models import Class, Language, Schedule, Timeslot, PurchaseHistory
 # Register your models here.
 
 admin.site.register(Class)
 admin.site.register(Language)
-admin.site.register(TypeOfClasses)
 admin.site.register(Schedule)
+admin.site.register(Timeslot)
+admin.site.register(PurchaseHistory)
