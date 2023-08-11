50917a320229eef270beecfecb36a2512d92438a
Seweryn Drag <dragseweryn@zohomail.eu>
2023-08-11 09:27:56 +0200

    Dodanie time-input-formats do pliku ustawień

--- backend/settings.py
+++ backend/settings.py
@@ -217,3 +217,5 @@ CITIES_LIGHT_INCLUDE_CITY_TYPES = ['PPL', 'PPLA', 'PPLA2', 'PPLA3',
                                    'PPLA4', 'PPLC', 'PPLF', 'PPLG', 'PPLL', 'PPLR', 'PPLS', 'STLMT']
 CITIES_LIGHT_CITY_SOURCES = [
     "http://download.geonames.org/export/dump/cities500.zip"]
+
+TIME_INPUT_FORMATS = ('%H:%M',)
