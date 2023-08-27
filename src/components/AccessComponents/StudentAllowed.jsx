2543d4c0a5856ceba339fad5775858296c025201
Seweryn Drag <dragseweryn@zohomail.eu>
2023-08-27 09:08:24 +0200

    Dodanie komponentów, które pozwalają na dostęp do danego widoku dla określonej roli uzytkownika

---
+++ src/components/AccessComponents/StudentAllowed.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
+
+const StudentAllowed = () => {
+  const { user } = useContext(AuthContext);
+
+  return user != null && user?.role == "Student" ? (
+    <>
+      <Outlet />
+    </>
+  ) : (
+    <>
+      <Navigate to="/" replace />
+    </>
+  );
+};
+
+export default StudentAllowed;
---
+++ src/components/AccessComponents/TeacherAllowed.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
+
+const TeacherAllowed = () => {
+  const { user } = useContext(AuthContext);
+
+  return user != null && user?.role == "Teacher" ? (
+    <>
+      <Outlet />
+    </>
+  ) : (
+    <>
+      <Navigate to="/" replace />
+    </>
+  );
+};
+
+export default TeacherAllowed;
