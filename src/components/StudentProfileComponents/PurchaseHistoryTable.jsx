eccbb7cae09a2a83e4f9fcf1d62544ae1c4b1846
Seweryn Drag <dragseweryn@zohomail.eu>
2023-09-01 15:41:37 +0200

    Stworzenie widoku z historią zakupów studenta.

---
+++ src/components/StudentProfileComponents/PurchaseHistoryTable.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import dayjs from "dayjs";
+import { Link } from "react-router-dom";
+const PurchaseHistoryTable = (props) => {
+  const { purchases } = props;
+
+  return (
+    <div className="container mx-auto text-gray-100 mb-10">
+      <div className="flex flex-col  text-xs">
+        <div className="flex items-center justify-center text-left bg-transparent text-gray-700 border-b border-opacity-60 border-gray-700">
+          <div
+            className="flex-1 px-2 py-3 sm:p-3 cursor-pointer hover:font-bold transition-all duration-300"
+            title="Sortuj po nazwie pliku"
+          >
+            Zajęcia
+          </div>
+          <div className="hidden w-24 px-2 py-3 sm:p-3 sm:block cursor-pointer hover:font-bold transition-all duration-300 text-center">
+            Pokój
+          </div>
+          <div className="w-20 sm:w-24 px-2 py-3 sm:p-3 block cursor-pointer hover:font-bold transition-all duration-300 text-center">
+            Liczba zajęć
+          </div>
+          <div className="w-20 sm:w-24 px-2 py-3 text-center sm:p-3 block cursor-pointer hover:font-bold transition-all duration-300">
+            Zapłacona kwota
+          </div>
+          <div className="hidden w-24 px-2 py-3 text-right sm:p-3 sm:block cursor-pointer hover:font-bold transition-all duration-300">
+            Data zakupu
+          </div>
+        </div>
+        {purchases.length == 0 && (
+          <span className="text-black italic text-center mt-3">
+            Brak plików.
+          </span>
+        )}
+        {purchases?.map((purchase) => (
+          <div
+            key={purchase.id}
+            className="flex border-b border-opacity-20 border-gray-700 bg-transparent text-black hover:bg-slate-100 transition-all duration-200"
+          >
+            <div className="flex flex-1 items-center px-2 py-3 sm:p-3 sm:w-auto cursor-pointer sm:truncate">
+              <Link
+                className="hover:underline transition-all duration-200 sm:truncate"
+                to={`/classes/${purchase?.classes?.id}`}
+                title={purchase?.classes?.name}
+              >
+                {purchase?.classes?.name}
+              </Link>
+            </div>
+            <div className="hidden w-24 px-2 py-3 sm:p-3 sm:flex justify-center items-center text-center">
+              <Link
+                className="underline"
+                to={`/pokoj/${purchase?.room?.room_id}`}
+              >
+                Link
+              </Link>
+            </div>
+            <div className="w-20 sm:w-24 px-2 py-3 sm:p-3 flex justify-center items-center text-center">
+              {purchase?.amount_of_lessons}
+            </div>
+            <div className="w-20 sm:w-24 px-2 py-3 sm:p-3 flex justify-center items-center text-center">
+              {purchase?.paid_price} zł
+            </div>
+            <div className="hidden w-24 px-2 py-3 text-right sm:p-3 sm:flex justify-center items-center text-gray-400">
+              <p>
+                {" "}
+                {dayjs(purchase?.purchase_date).format("YYYY-MM-DD, HH:mm")}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PurchaseHistoryTable;
---
+++ src/pages/PurchaseHistoryPage.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect, useContext } from "react";
+import useAxios from "../utils/useAxios";
+import LoadingComponent from "../components/LoadingComponent";
+import Pagination from "../components/Pagination";
+import PurchaseHistoryTable from "../components/StudentProfileComponents/PurchaseHistoryTable";
+
+const PurchaseHistoryPage = () => {
+  const [loading, setLoading] = useState(true);
+  const [totalPages, setTotalPages] = useState(0);
+  const [totalResults, setTotalResults] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [purchases, setPurchases] = useState([]);
+  const api = useAxios();
+
+  const fetchHistory = async () => {
+    setLoading(true);
+    await api
+      .get(`api/classes/purchase-classes/history/?page_size=10`)
+      .then((res) => {
+        console.log(res);
+        if (res.data.results == null) {
+          setPurchases(null);
+          setTotalPages(0);
+          setTotalResults(0);
+          setCurrentPage(1);
+        } else {
+          setPurchases(res.data.results);
+          setTotalPages(res.data.num_pages);
+          setTotalResults(res.data.count);
+          setCurrentPage(res.data.current_page);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setLoading(false);
+  };
+
+  const searchPurchases = async (page) => {
+    setLoading(true);
+    let baseurl = `api/classes/purchase-classes/history/?page_size=10&page=${page}`;
+
+    await api
+      .get(baseurl)
+      .then((res) => {
+        console.log(res);
+        if (res.data.results == null) {
+          setPurchases(null);
+          setTotalPages(0);
+          setTotalResults(0);
+          setCurrentPage(1);
+        } else {
+          setPurchases(res.data.results);
+          setTotalPages(res.data.num_pages);
+          setTotalResults(res.data.count);
+          setCurrentPage(res.data.current_page);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchHistory();
+  }, []);
+
+  return (
+    <div className="flex flex-col">
+      <div className="absolute top-[70px] left-0 right-0 h-[300px] bg-base-300 "></div>
+      {loading ? (
+        <LoadingComponent message="Ładowanie informacji o plikach..." />
+      ) : (
+        <>
+          <div className="md:text-2xl max-md:text-xl max-phone:text-lg card bg-white rounded-none mb-5 text-center p-4 border-[1px] border-base-200 flex flex-row justify-between items-center z-30  mt-10 shadow-xl">
+            <h1 className="text-center w-full">Historia zakuów</h1>
+          </div>
+          <div className="card rounded-none bg-white p-6 shadow-xl">
+            <PurchaseHistoryTable purchases={purchases} />
+            {totalPages > 1 && (
+              <Pagination
+                totalResults={totalResults}
+                totalPages={totalPages}
+                currentPage={currentPage}
+                search={searchPurchases}
+              />
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default PurchaseHistoryPage;
