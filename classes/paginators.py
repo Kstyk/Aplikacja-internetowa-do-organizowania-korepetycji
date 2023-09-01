39a18b9daf86bf0849dd04b0342fcf9ccb979c55
Seweryn Drag <dragseweryn@zohomail.eu>
2023-09-01 15:52:00 +0200

    Stworzenie paginatora do rezultatu zwracanych historii zakupów

--- classes/paginators.py
+++ classes/paginators.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class ClassPagination(PageNumberPagination):
@@ -6,3 +7,20 @@ class ClassPagination(PageNumberPagination):
     page_size_query_param = "page_size"
     page_query_param = 'page'
     max_page_size = 100
+
+
+class PurchaseHistoryPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    page_query_param = 'page'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'num_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'results': data,
+        })
