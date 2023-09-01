b4926dd673a67411506503d9b9f1a58fb66b16d5
Seweryn Drag <dragseweryn@zohomail.eu>
2023-09-01 15:51:36 +0200

    Czytellniejsze wyswietlanie obiektów schedule w django admin

--- classes/admin.py
+++ classes/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Class, Language, Schedule, Timeslot, PurchaseHistory
 # Register your models here.
 
+
+class ScheduleAdmin(admin.ModelAdmin):
+    model = Schedule
+    list_display = ('id', 'date', 'student', 'classes')
+
+
 admin.site.register(Class)
 admin.site.register(Language)
-admin.site.register(Schedule)
+admin.site.register(Schedule, ScheduleAdmin)
 admin.site.register(Timeslot)
 admin.site.register(PurchaseHistory)
