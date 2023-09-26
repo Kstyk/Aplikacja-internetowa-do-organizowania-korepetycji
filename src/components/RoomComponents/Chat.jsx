2acbb8865ebb83f423a0659ee8e13278cda93e40
Seweryn Drag <dragseweryn@zohomail.eu>
2023-09-26 18:45:15 +0200

    Usprawnienie widoku wideocalla, dodanie mozliwosci czatowania bezposrtednio z wideocalla

--- src/components/RoomComponents/Chat.jsx
+++ src/components/RoomComponents/Chat.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState, useId } from 'react'
 import AuthContext from '../../context/AuthContext'
 import useWebSocket, { ReadyState } from 'react-use-websocket'
 import InfiniteScroll from 'react-infinite-scroll-component'
+import DetectRTC from 'detectrtc'
 import { useNavigate, useParams } from 'react-router-dom'
 import Message from './Message'
 import ChatLoader from './ChatLoader'
@@ -15,6 +16,14 @@ import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
 import { AiOutlineSend } from 'react-icons/ai'
 import { FiPhoneCall } from 'react-icons/fi'
 import { FcCallback, FcEndCall } from 'react-icons/fc'
+import {
+  BiMicrophoneOff,
+  BiMicrophone,
+  BiCameraOff,
+  BiCamera,
+} from 'react-icons/bi'
+import { RiChat4Line, RiChatOffLine } from 'react-icons/ri'
+import { LuScreenShareOff, LuScreenShare } from 'react-icons/lu'
 import showAlertError from '../messages/SwalAlertError'
 
 const Chat = ({ archivized }) => {
@@ -42,6 +51,8 @@ const Chat = ({ archivized }) => {
   const [audioEnabled, setAudioEnabled] = useState(true)
   const [isCameraOn, setIsCameraOn] = useState(true)
   const [isScreenSharing, setIsScreenSharing] = useState(false)
+  const [showChat, setShowChat] = useState(false)
+
   const { readyState, sendJsonMessage } = useWebSocket(
     user ? `ws://127.0.0.1:8000/chats/${roomId}/` : null,
     {
@@ -165,8 +176,13 @@ const Chat = ({ archivized }) => {
       const hasMicrophone = devices.some(
         (device) => device.kind === 'audioinput'
       )
-      const hasCamera = devices.some((device) => device.kind === 'videoinput')
-
+      const hasCamera = devices.some(
+        (device) =>
+          device.kind === 'videoinput' &&
+          device.deviceId != '' &&
+          device.deviceId != null
+      )
+      console.log(devices)
       // Tutaj możesz podjąć odpowiednie działania w zależności od wyników
       // TODO
       if (!hasMicrophone) {
@@ -200,20 +216,23 @@ const Chat = ({ archivized }) => {
         navigator.getUserMedia ||
         navigator.webkitGetUserMedia ||
         navigator.mozGetUserMedia
-
-      getUserMedia({ video: true, audio: true }, (mediaStream) => {
-        currentUserVideoRef.current.srcObject = mediaStream
-        currentUserVideoRef.current.onloadedmetadata = () => {
-          currentUserVideoRef.current.play()
-        }
-        call.answer(mediaStream)
-        call.on('stream', function (remoteStream) {
-          remoteVideoRef.current.srcObject = remoteStream
-          remoteVideoRef.current.onloadedmetadata = () => {
-            remoteVideoRef.current.play()
+      if (getUserMedia) {
+        getUserMedia({ video: true, audio: true }, (mediaStream) => {
+          currentUserVideoRef.current.srcObject = mediaStream
+          currentUserVideoRef.current.onloadedmetadata = () => {
+            currentUserVideoRef.current.play()
           }
+          call.answer(mediaStream)
+          call.on('stream', function (remoteStream) {
+            remoteVideoRef.current.srcObject = remoteStream
+            remoteVideoRef.current.onloadedmetadata = () => {
+              remoteVideoRef.current.play()
+            }
+          })
         })
-      })
+      } else {
+        console.log('no cam')
+      }
     })
 
     peer.on('close', () => {
@@ -229,31 +248,34 @@ const Chat = ({ archivized }) => {
       navigator.webkitGetUserMedia ||
       navigator.mozGetUserMedia
 
-    getUserMedia({ video: true, audio: true }, (mediaStream) => {
-      currentUserVideoRef.current.srcObject = mediaStream
-      currentUserVideoRef.current.onloadedmetadata = () => {
-        currentUserVideoRef.current.play()
-      }
-
-      const call = peerInstance.current.call(remotePeerId, mediaStream, {
-        metadata: {
-          callerPeerId: peerId,
-        },
-      })
-
-      call.on(
-        'stream',
-        (remoteStream) => {
-          remoteVideoRef.current.srcObject = remoteStream
-          remoteVideoRef.current.onloadedmetadata = () => {
-            remoteVideoRef.current.play()
-          }
-        },
-        function (err) {
-          showAlertError('Błąd', 'Błąd przy strumieniowaniu obrazu.')
+    if (getUserMedia) {
+      console.log(DetectRTC.hasMicrophone)
+      getUserMedia({ video: true, audio: true }, (mediaStream) => {
+        currentUserVideoRef.current.srcObject = mediaStream
+        currentUserVideoRef.current.onloadedmetadata = () => {
+          currentUserVideoRef.current.play()
         }
-      )
-    })
+
+        const call = peerInstance.current.call(remotePeerId, mediaStream, {
+          metadata: {
+            callerPeerId: peerId,
+          },
+        })
+
+        call.on(
+          'stream',
+          (remoteStream) => {
+            remoteVideoRef.current.srcObject = remoteStream
+            remoteVideoRef.current.onloadedmetadata = () => {
+              remoteVideoRef.current.play()
+            }
+          },
+          function (err) {
+            showAlertError('Błąd', 'Błąd przy strumieniowaniu obrazu.')
+          }
+        )
+      })
+    }
   }
 
   const rejectVideoCall = async () => {
@@ -381,7 +403,10 @@ const Chat = ({ archivized }) => {
     <>
       <div className="flex h-full flex-col">
         <div className="!z-10 flex h-full flex-col justify-between">
-          <div id="scrollableDiv" className="w-full">
+          <div
+            id="scrollableDiv"
+            className="card fixed bottom-20 top-[16rem] flex w-[calc(100%-24px)] flex-col-reverse overflow-y-auto rounded-none border border-none border-gray-200 bg-white p-6 shadow-xl phone:w-11/12 phone:border-t-2 md:w-10/12 lg:w-8/12"
+          >
             {readyState === ReadyState.CONNECTING ? (
               <LoadingComponent message="Ładowanie wiadomości..." />
             ) : (
@@ -391,7 +416,7 @@ const Chat = ({ archivized }) => {
                     <InfiniteScroll
                       dataLength={messageHistory.length}
                       next={fetchMessages}
-                      className="fixed bottom-20 top-[16rem] flex w-[calc(100%-24px)] flex-col-reverse bg-white pt-2 phone:w-11/12 phone:border-t-2 md:w-10/12 lg:w-8/12"
+                      className="flex flex-col-reverse"
                       inverse={true}
                       hasMore={hasMoreMessages}
                       loader={<ChatLoader />}
@@ -406,80 +431,80 @@ const Chat = ({ archivized }) => {
                       ))}
                     </InfiniteScroll>
                   ) : (
-                    <div className="h-full w-full text-center italic">
+                    <div className="bottom-20 top-[16rem] mt-5 flex h-[60vh] w-full justify-center bg-white pt-5 text-center italic text-black">
                       Brak nowych wiadomości.
-                    </div>
-                  )}
-                  {archivized ? (
-                    <div className="right- fixed bottom-0 mx-auto flex w-full flex-row items-center border-t-2 bg-white p-5 phone:w-11/12 md:w-10/12 lg:w-8/12">
-                      <div className="flex h-10 w-full items-center justify-center italic text-gray-600">
-                        <span>Historia czatu</span>
-                      </div>
-                    </div>
-                  ) : (
-                    <div className="fixed bottom-0 mx-auto flex w-[calc(100%-24px)] flex-row items-center justify-between border-t-2 bg-white p-5 max-phone:mr-3 phone:w-11/12 md:w-10/12 lg:w-8/12">
-                      <div className="flex h-10 w-10/12 items-center justify-between border-b-2">
-                        <input
-                          type="text"
-                          name="message"
-                          placeholder="Napisz wiadomość..."
-                          onChange={handleChangeMessage}
-                          value={message}
-                          className="h-full w-11/12 pl-3 outline-none"
-                        />
-                        <button
-                          className="tooltip ml-2 h-10 px-3 py-1"
-                          data-tip="Wyślij wiadomość"
-                          onClick={handleSubmit}
-                        >
-                          <AiOutlineSend className="h-5 w-5 phone:h-6 phone:w-6" />
-                        </button>
-                      </div>
-
-                      <>
-                        <button
-                          className="ml-3px-3 tooltip h-10 py-1"
-                          data-tip="Zadzwoń"
-                          onClick={() => startVideoCall()}
-                        >
-                          <FiPhoneCall className="h-5 w-5 phone:h-6 phone:w-6" />
-                        </button>
-                        {callButton == true ||
-                        localStorage.getItem('callButton') == 'true' ? (
-                          <button
-                            className="tooltip ml-3 h-10 py-1"
-                            data-tip="Odbierz połączenie"
-                            onClick={(e) => openModal()}
-                          >
-                            <FcCallback className="h-7 w-7 phone:h-8 phone:w-8" />
-                          </button>
-                        ) : (
-                          ''
-                        )}
-                        {callButton == true ||
-                        localStorage.getItem('callButton') == 'true' ? (
-                          <button
-                            className="tooltip ml-3 h-10 py-1"
-                            data-tip="Odrzuć połączenie"
-                            onClick={(e) => {
-                              localStorage.removeItem('callButton')
-                              localStorage.removeItem('peerId')
-                              setCallButton(null)
-                              rejectVideoCall()
-                            }}
-                          >
-                            <FcEndCall className="h-7 w-7 phone:h-8 phone:w-8" />
-                          </button>
-                        ) : (
-                          ''
-                        )}
-                      </>
                     </div>
                   )}
                 </>
               </>
             )}
           </div>
+          {archivized ? (
+            <div className="right- fixed bottom-0 mx-auto flex w-full flex-row items-center border-t-2 bg-white p-5 phone:w-11/12 md:w-10/12 lg:w-8/12">
+              <div className="flex h-10 w-full items-center justify-center italic text-gray-600">
+                <span>Historia czatu</span>
+              </div>
+            </div>
+          ) : (
+            <div className="fixed bottom-0 mx-auto flex w-[calc(100%-24px)] flex-row items-center justify-between border-t-2 bg-white p-5 max-phone:mr-3 phone:w-11/12 md:w-10/12 lg:w-8/12">
+              <div className="flex h-10 w-10/12 items-center justify-between border-b-2">
+                <input
+                  type="text"
+                  name="message"
+                  placeholder="Napisz wiadomość..."
+                  onChange={handleChangeMessage}
+                  value={message}
+                  className="h-full w-11/12 pl-3 outline-none"
+                />
+                <button
+                  className="tooltip ml-2 h-10 px-3 py-1"
+                  data-tip="Wyślij wiadomość"
+                  onClick={handleSubmit}
+                >
+                  <AiOutlineSend className="h-5 w-5 phone:h-6 phone:w-6" />
+                </button>
+              </div>
+
+              <>
+                <button
+                  className="ml-3px-3 tooltip h-10 py-1"
+                  data-tip="Zadzwoń"
+                  onClick={() => startVideoCall()}
+                >
+                  <FiPhoneCall className="h-5 w-5 phone:h-6 phone:w-6" />
+                </button>
+                {callButton == true ||
+                localStorage.getItem('callButton') == 'true' ? (
+                  <button
+                    className="tooltip ml-3 h-10 py-1"
+                    data-tip="Odbierz połączenie"
+                    onClick={(e) => openModal()}
+                  >
+                    <FcCallback className="h-7 w-7 phone:h-8 phone:w-8" />
+                  </button>
+                ) : (
+                  ''
+                )}
+                {callButton == true ||
+                localStorage.getItem('callButton') == 'true' ? (
+                  <button
+                    className="tooltip ml-3 h-10 py-1"
+                    data-tip="Odrzuć połączenie"
+                    onClick={(e) => {
+                      localStorage.removeItem('callButton')
+                      localStorage.removeItem('peerId')
+                      setCallButton(null)
+                      rejectVideoCall()
+                    }}
+                  >
+                    <FcEndCall className="h-7 w-7 phone:h-8 phone:w-8" />
+                  </button>
+                ) : (
+                  ''
+                )}
+              </>
+            </div>
+          )}
           {/* </div> */}
         </div>
 
@@ -506,7 +531,7 @@ const Chat = ({ archivized }) => {
             </TransformComponent>
           </TransformWrapper>
         </div>
-        <div className="absolute top-0 flex w-full flex-row justify-center gap-x-4 bg-black text-white">
+        <div className="absolute top-0 flex w-full flex-row items-center justify-center gap-x-4 bg-black text-white">
           <form method="dialog m-0 p-0 min-h-0" id="closeCallForm">
             <div className="modal-action m-0 p-0">
               <button
@@ -514,36 +539,125 @@ const Chat = ({ archivized }) => {
                   rejectVideoCall()
                   endVideoCall()
                 }}
-                className="modal-action m-0 flex h-1/4 items-center justify-center border-b-2 border-white p-0 hover:bg-slate-700"
+                className="modal-action tooltip tooltip-bottom m-0 flex h-1/4 items-center justify-center rounded-full p-2  hover:bg-slate-700"
+                data-tip="Zakończ połączenie"
               >
-                Zakończ połączenie
+                <FcEndCall className="h-7 w-7 phone:h-8 phone:w-8" />
               </button>
             </div>
           </form>
           <div
             onClick={() => toggleCamera()}
-            className="flex h-1/4 items-center justify-center border-b-2 border-white hover:bg-slate-700"
+            className="tooltip tooltip-bottom flex h-1/4 items-center justify-center rounded-full p-2 hover:bg-slate-700"
+            data-tip={isCameraOn ? 'Kamera włączona' : 'Kamera wyłączona'}
           >
-            {isCameraOn ? 'Wyłącz' : 'Włącz'} kamerę
+            {isCameraOn ? (
+              <BiCamera className="h-7 w-7 phone:h-8 phone:w-8" />
+            ) : (
+              <BiCameraOff className="h-7 w-7 phone:h-8 phone:w-8" />
+            )}
           </div>
           <div
             onClick={() => toggleAudio()}
-            className="flex h-1/4 items-center justify-center border-b-2 border-white hover:bg-slate-700"
+            className="tooltip tooltip-bottom flex h-1/4 items-center justify-center rounded-full p-2 hover:bg-slate-700"
+            data-tip={audioEnabled ? 'Mikrofon włączony' : 'Mikrofon wyłączony'}
           >
-            {audioEnabled ? 'Wyłącz' : 'Włącz'} mikrofon
+            {audioEnabled ? (
+              <BiMicrophone className="h-7 w-7 phone:h-8 phone:w-8" />
+            ) : (
+              <BiMicrophoneOff className="h-7 w-7 phone:h-8 phone:w-8" />
+            )}
           </div>
           <div
             onClick={() => toggleScreenSharing()}
-            className="flex h-1/4 items-center justify-center hover:bg-slate-700"
+            className="tooltip tooltip-bottom flex h-1/4 items-center justify-center rounded-full p-2 hover:bg-slate-700"
+            data-tip={
+              isScreenSharing ? 'Zakończ udostępnianie' : 'Udostępnij ekran'
+            }
           >
-            {isScreenSharing ? 'Zakończ udostępnianie' : 'Udostępnij ekran'}
+            {isScreenSharing ? (
+              <LuScreenShareOff className="h-7 w-7 phone:h-8 phone:w-8" />
+            ) : (
+              <LuScreenShare className="h-7 w-7 phone:h-8 phone:w-8" />
+            )}
+          </div>
+          <div
+            onClick={() => setShowChat((prev) => !prev)}
+            className="tooltip tooltip-bottom flex h-1/4 items-center justify-center rounded-full p-2 hover:bg-slate-700"
+            data-tip={showChat ? 'Ukryj czat' : 'Pokaż czat'}
+          >
+            {showChat ? (
+              <RiChatOffLine className="h-8 w-8 phone:h-9 phone:w-9" />
+            ) : (
+              <RiChat4Line className="h-8 w-8 phone:h-9 phone:w-9" />
+            )}
           </div>
         </div>
         <video
           preload="none"
           ref={currentUserVideoRef}
-          className="absolute bottom-0 left-0 w-2/12"
+          className="absolute bottom-0 left-0 w-6/12 rounded-tr-md shadow-2xl shadow-gray-500 phone:w-4/12 sm:w-2/12"
         />
+        <div
+          id="scrollableDivVideoCall"
+          className={
+            showChat
+              ? `card fixed bottom-20 right-0 top-[54px] flex w-10/12 flex-col-reverse overflow-y-auto rounded-none border border-none border-gray-200 bg-white py-6 shadow-xl sm:w-7/12 md:w-5/12 lg:w-4/12 xl:w-3/12`
+              : 'hidden'
+          }
+        >
+          {readyState === ReadyState.CONNECTING ? (
+            <LoadingComponent message="Ładowanie wiadomości..." />
+          ) : (
+            <>
+              <>
+                {messageHistory.length != 0 ? (
+                  <InfiniteScroll
+                    dataLength={messageHistory.length}
+                    next={fetchMessages}
+                    className="flex w-full flex-col-reverse bg-white pt-2"
+                    inverse={true}
+                    hasMore={hasMoreMessages}
+                    loader={<ChatLoader />}
+                    scrollableTarget="scrollableDivVideoCall"
+                  >
+                    {messageHistory.map((message) => (
+                      <Message
+                        key={message.id}
+                        message={message}
+                        secondUser={secondUserProfile}
+                      />
+                    ))}
+                  </InfiniteScroll>
+                ) : (
+                  <div className="mt-5 flex h-[60vh] w-full justify-center bg-white pt-5 text-center italic text-black">
+                    Brak nowych wiadomości.
+                  </div>
+                )}
+
+                <div className="fixed bottom-0 right-0 mx-auto flex h-[10vh] w-10/12 flex-row items-center justify-between border-t-2 bg-white p-5 sm:w-7/12 md:w-5/12 lg:w-4/12 xl:w-3/12">
+                  <div className="flex h-10 w-10/12 items-center justify-between border-b-2">
+                    <input
+                      type="text"
+                      name="message"
+                      placeholder="Napisz wiadomość..."
+                      onChange={handleChangeMessage}
+                      value={message}
+                      className="h-full w-11/12 pl-3 outline-none"
+                    />
+                    <button
+                      className="tooltip ml-2 h-10 px-3 py-1"
+                      data-tip="Wyślij wiadomość"
+                      onClick={handleSubmit}
+                    >
+                      <AiOutlineSend className="h-5 w-5 phone:h-6 phone:w-6" />
+                    </button>
+                  </div>
+                </div>
+              </>
+            </>
+          )}
+        </div>
       </dialog>
     </>
   )
