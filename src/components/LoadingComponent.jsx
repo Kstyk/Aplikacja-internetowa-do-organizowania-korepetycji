8fad6c37de48fe42fb81dd40ba449740c23d11e3
Seweryn Drag <dragseweryn@zohomail.eu>
2023-10-14 22:11:29 +0200

    Usuniecie mt i pt

--- src/components/LoadingComponent.jsx
+++ src/components/LoadingComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoadingComponent = (props) => {
   return (
-    <div className="mt-10 flex w-full flex-col items-center justify-center bg-inherit pt-10">
+    <div className="flex w-full flex-col items-center justify-center bg-transparent">
       <span className="loading loading-spinner loading-lg text-base-400"></span>
       <span className="text-center text-black">{props.message}</span>
     </div>
